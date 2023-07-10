961cbe6babf71c3757e15c12654d3f6389888986
joao-arthr <jao.arthurag@gmail.com>
2023-07-10 14:39:43 -0300

    build:installing dependencies

---
+++ src/test/java/br/com/compass/challenge2/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.challenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
