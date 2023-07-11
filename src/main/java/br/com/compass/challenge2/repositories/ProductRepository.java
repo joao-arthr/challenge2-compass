f5f082a801bfb5572bab74e17e89869b182234ee
Rhauan Masulim <rmasulim@gmail.com>
2023-07-11 17:34:09 -0300

    feat:productRepository added

---
+++ src/main/java/br/com/compass/challenge2/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.compass.challenge2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.compass.challenge2.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
