c8851034c688c4291e64409f68c584e7c652dbe2
Letícia Sayuri Imasato <99757921+lesyimasato@users.noreply.github.com>
2023-07-13 14:02:48 -0300

    Update Product.java

--- src/main/java/br/com/compass/challenge2/model/Product.java
+++ src/main/java/br/com/compass/challenge2/model/Product.java
@@ -1,37 +1,45 @@
 package br.com.compass.challenge2.model;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="Soda")
 public class Product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Column(name="sodaId")
+	private int id;
+
+	@Column(name="soda_name")
 	private String name;
+
+	@Column(name="price")
 	private double price;
+
+	@Column(name="quantity")
+	private int quantity;
 	
-	public Product() {
+	public Product(int id, String name, double price, int quantity) {
 		
 	}
-	
-	
-	public Product(Long id, String name, double price) {
-		this.id = id;
+
+	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
+		this.quantity = quantity;
 	}
 
-
-
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
@@ -50,6 +58,22 @@ public class Product {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", price=" + price +
+				", quantity=" + quantity +
+				'}';
+	}
 }
