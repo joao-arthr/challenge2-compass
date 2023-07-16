ac79415ab4e18e7ece0051b62e6fff8777f50fd3
joao-arthr <jao.arthurag@gmail.com>
2023-07-16 15:37:00 -0300

    refactor: Remove unused SQL files

--- src/main/java/br/com/compass/challenge2/controller/ProductController.java
+++ src/main/java/br/com/compass/challenge2/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import br.com.compass.challenge2.exception.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
