a9acf2047e4b82065c583f261a4d25f505cfd8c2
joao-arthr <jao.arthurag@gmail.com>
2023-07-16 14:51:44 -0300

    style: minor changes

--- src/main/java/br/com/compass/challenge2/controller/ProductController.java
+++ src/main/java/br/com/compass/challenge2/controller/ProductController.java
@@ -25,6 +25,7 @@ public class ProductController {
 
 	private final ProductService productService;
 
+
 	@Autowired
 	public ProductController(ProductService productService) {
 		this.productService = productService;
